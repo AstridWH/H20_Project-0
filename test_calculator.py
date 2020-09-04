c686108d0d1ea6db7edce4a1f1ac58271c71e084
AstridWH <70654578+AstridWH@users.noreply.github.com>
2020-09-04 20:04:11 +0200

    Did exersise 2

--- test_calculator.py
+++ test_calculator.py
@@ -3,5 +3,15 @@ from calculator import add
 def test_add(a=1, b=2):
     n = add(a, b)
     m = a + b
-    assert n == m 
+    return n == m 
     
+def test_add_float(a=0.1, b=0.2):
+    #print(add(a,b))
+    #print(a+b)
+    return abs((add(a,b)) - (a+b)) < 1e-5
+
+    
+print(test_add())
+print(test_add(0.1, 0.2))   #Samme resultat som add_float
+print(test_add_float())
+
